e36d84988898660f49a70c81145b8cf3787a1a55
Yenulii <yenulivt@gmail.com>
2026-04-13 18:24:25 +0530

    Added dashboard logic to ModuleLeaderController

--- Controllers/ModuleLeaderController.cs
+++ Controllers/ModuleLeaderController.cs
@@ -1,8 +1,33 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using BlindMatchPAS.Data;
+using Microsoft.EntityFrameworkCore;
 
-public class Class1
+namespace BlindMatchPAS.Controllers
 {
-	public Class1()
-	{
-	}
+    public class ModuleLeaderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ModuleLeaderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            var totalProjects = await _context.Projects.CountAsync();
+            var totalMatches = await _context.Matches.CountAsync();
+            var pendingProjects = await _context.Projects
+                .Where(p => p.Status == "Pending")
+                .CountAsync();
+            var totalUsers = await _context.Users.CountAsync();
+
+            ViewBag.TotalProjects = totalProjects;
+            ViewBag.TotalMatches = totalMatches;
+            ViewBag.PendingProjects = pendingProjects;
+            ViewBag.TotalUsers = totalUsers;
+
+            return View();
+        }
+    }
 }
