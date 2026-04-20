c7342ba9bde80eea73b154a5b803b89c669b420c
LinalDevdun <dlrdandradi@students.nsbm.ac.lk>
2026-04-20 23:40:11 +0530

    Add CompatibilityScore property to project model

--- Models/ProjectProposal.cs
+++ Models/ProjectProposal.cs
@@ -3,9 +3,11 @@ namespace BlindMatchPAS.Models
     public class ProjectProposal
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Abstract { get; set; }
-        public string TechStack { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Abstract { get; set; } = string.Empty;
+        public string TechStack { get; set; } = string.Empty;
         public int CompatibilityScore { get; set; }
+        public DateTime SubmittedDate { get; set; }
+        public ICollection<ResearchArea> ResearchAreas { get; set; } = new List<ResearchArea>();
     }
 }
